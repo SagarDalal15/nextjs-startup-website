9b14ecc212f1c36659ea0ae268f2c0d7c8444035
Sagar Dalal <sagardalal112@gmail.com>
2021-07-25 18:28:24 +0530

    Github Link added

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "../styles/globals.css";
 import NavigationBar from "../components/navigation-bar";
+import GithubLink from "../components/github-link";
 
 function MyApp({ Component, pageProps }) {
   return (
     <NavigationBar>
       <Component {...pageProps} />
+      <GithubLink />
     </NavigationBar>
   );
 }
